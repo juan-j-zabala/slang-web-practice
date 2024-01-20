c779d906a1bac8473a5213184d92b91706cd7c77
juan-j-zabala <juzadev217@gmail.com>
2024-01-20 16:47:24 -0500

    bento ended and prefooter added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,23 @@
-import animations from 'tailwindcss-animated'
+import animations from "tailwindcss-animated";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      onest: ["Onest Variable"],
+    },
+    extend: {
+      backgroundImage: {
+        mountain: "url('/mountain.webp')",
+        gref: "url('/grefposing.webp')",
+        pista: "url('/pistas.webp')",
+        compu: "url('/laptop.webp')",
+      },
+      backgroundColor: {
+        "bg-base": "#020710",
+      },
+    },
   },
   plugins: [animations],
-}
+};
